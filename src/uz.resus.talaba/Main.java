f6306d2bc172909d7fee262f49398246503cb762
Akmal Quldoshev <akmal.quldoshov.97@gmail.com>
2021-06-16 19:34:06 +0500

    soutga o'zgarish

--- src/uz.resus.talaba/Main.java
+++ src/uz.resus.talaba/Main.java
@@ -2,6 +2,6 @@ package uz.resus.talaba;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kalom");
+        System.out.println("Akmaldan o'zgarish");
     }
 }
